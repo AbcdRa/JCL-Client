03959a9c951c7105d652c0f0e6c62225edc46880
AbcdRa <abdraloguzov@mail.ru>
2019-12-20 01:29:40 +0500

    Классическая реализация

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+        private static Socket socket;
+        private static Scanner scan = new Scanner(System.in);
+        private static String name;
+        private static BufferedWriter outBW;
+        private static String ip = "localhost";
+        private static Process process = null;
+
+        //Инициализируем сокет
+        public static boolean initSocket() {
+            while(true) {
+                int port = 1234;
+                try {
+                    socket = new Socket(ip, port);
+                    return true;
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                    System.out.println("IP: ");
+                    ip = scan.nextLine();
+                    if(ip.compareToIgnoreCase("0") == 0) return false;
+                    System.out.println("Поменять порт ? ");
+                    if(scan.nextLine().compareToIgnoreCase("да") == 0) {
+                        System.out.println("Port: ");
+                        try {
+                            port = Integer.parseInt(scan.nextLine());
+                        } catch(NumberFormatException e) {
+                            port = 1234;
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        public static void main(String[] args) {
+            if(!initSocket()) {
+                scan.close();
+                System.out.println("Не удалось найти доступный сервер");
+                return;
+            }
+            try {
+                OutputStream out = socket.getOutputStream();
+                OutputStreamWriter outW = new OutputStreamWriter(out);
+                outBW = new BufferedWriter(outW);
+            } catch (IOException e1) {
+                System.out.println("Не удалось загенерить выходной поток");
+                e1.printStackTrace();
+            }
+            System.out.println("Вроде подключились! ");
+            new Updater(socket);
+            //Отправляем служебное сообщение с паролем
+            //Включаем прослушку сообщений с сервера
+            String word;
+            while(true) {
+                try {
+                    //Ждем пока клиент что-нибудь напишет
+                    word = scan.nextLine();
+                    if(word.compareToIgnoreCase("end") == 0) break;
+//	        	if(word.compareToIgnoreCase("clear") == 0) clear();
+                    System.out.println("ОТЛАДКА: Отправляю " + word);
+                    outBW.write(word+"\n");
+                    //Отправляем сообщение на сервер
+                    outBW.flush();
+                } catch (IOException e) {
+                    System.out.println("Вас отключили от сервера");
+                    break;
+                }
+            }
+            System.out.println("Завершение сеанса");
+
+        }
+
+
+
+
+}
+
---
+++ src/Updater.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Updater extends Thread {
+    private Socket socket;
+    private BufferedReader inBR;
+    Updater(Socket socket) {
+        this.socket = socket;
+        start();
+    }
+
+    public void run() {
+        try {
+            InputStream in = socket.getInputStream();
+            InputStreamReader inR = new InputStreamReader(in);
+            inBR = new BufferedReader(inR);
+        } catch (IOException e1) {
+            System.out.println("Не удалось создать входной поток");
+            return;
+        }
+        while(true) {
+            try {
+                System.out.println(inBR.readLine());
+            } catch ( IOException e) {
+                System.out.println("Не удалось получить сообщение с сервера");
+                break;
+            }
+        }
+    }
+
+}
